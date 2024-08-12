9e250e09caa68c7394cf5170f2a80e915597683a
Alexapod123 <alex_krim99@mail.ru>
2024-08-12 19:20:55 +0300

    famiy tree

---
+++ family_tree/Main.java
@@ -0,0 +1,27 @@
+package family_tree;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree tree = testTree();
+        System.out.println(tree);
+    }
+
+    static FamilyTree testTree() {
+        FamilyTree tree = new FamilyTree();
+
+        Human vanya = new Human("Ivan", Gender.Male, LocalDate.of(1975, 6, 02), null, null, null);
+        Human alena = new Human("Alena", Gender.Female, LocalDate.of(1980, 7, 03), null, null, null);
+        tree.add(vanya);
+        tree.add(alena);
+        Human kira = new Human("Kira", Gender.Female, LocalDate.of(1999, 8, 5), null, alena, vanya);
+        Human igor = new Human("Igor", Gender.Male, LocalDate.of(2002, 3, 16), null, alena, vanya);
+        tree.add(kira);
+        tree.add(igor);
+        Human grandMother = new Human("Victoria", Gender.Female, LocalDate.of(1956, 6, 02), null, null, null);
+        grandMother.addChild(alena);
+        tree.add(grandMother);
+        return tree;
+    }
+}
